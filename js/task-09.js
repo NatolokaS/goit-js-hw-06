933f30e64726840e65849977d218a9c244152da6
NatolokaS <129997302+NatolokaS@users.noreply.github.com>
2023-08-14 01:44:41 +0300

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,18 @@
+const bodyElement = document.querySelector("body");
+const buttonElement = document.querySelector("button.change-color");
+const spanElement = document.querySelector("span.color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonHandler = () => {
+  bodyElement.style.backgroundColor = getRandomHexColor();
+};
+
+buttonElement.addEventListener("click", buttonHandler);
+
+
+
